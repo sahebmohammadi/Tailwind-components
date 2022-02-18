8f0939d78f373cc674c0348851d6f98a9f3fa901
saheb mohammadi <saheb.put92@gmail.com>
2022-02-18 15:37:32 +0330

    add dropdown & pagination

---
+++ DropDown/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ Pagination/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        22: "5.5rem",
+      },
+      inset: {
+        18: "4.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
